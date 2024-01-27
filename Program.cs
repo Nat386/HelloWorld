0588604936312b4058b89ea734b2850907841188
Nat386 <n.karasyova53@gmail.com>
2024-01-26 22:52:00 -0600

    added variables

--- Program.cs
+++ Program.cs
@@ -9,3 +9,20 @@ Console.WriteLine(yourName);
 yourName = "bob";
 
 Console.WriteLine(yourName);
+
+bool day = false;
+bool night = true;
+
+int weight = 123;
+int height = 55;
+
+long distance = 92346484;
+long time = -372587958;
+
+char char1 = 'w';
+char char2 = 'h';
+
+string monthofbirth = "april";
+string traveldestination = "madrid";
+
+
