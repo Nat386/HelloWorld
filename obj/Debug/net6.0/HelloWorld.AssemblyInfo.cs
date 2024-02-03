4921d17fa3fe4643b9b4c3d5b864a73342590d85
Nat386 <n.karasyova53@gmail.com>
2024-02-03 12:11:36 -0600

    deleted files

--- obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c655da5d2c059786429e50aeabec09a92f3919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cde320657b94a51ae353761cee72a521b5cee3ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
