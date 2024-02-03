566ecd0a4563f7f24dcd306d535355e74682169d
Nat386 <n.karasyova53@gmail.com>
2024-02-02 20:52:47 -0600

    console-questions-training

--- Program.cs
+++ Program.cs
@@ -1,51 +1,13 @@
-﻿int weight = 123 + 55;
-Console.WriteLine(weight);
 
-int distance = 4568 - weight;
-Console.WriteLine(distance);
-
-int example = 78546 / 16;
-Console.WriteLine(example);
-
-int example2 = 56 * 3;
-Console.WriteLine(example2);
-
-int digit = 4;
-digit++;
-Console.WriteLine(digit);
-
-digit--;
-    Console.WriteLine(digit);
-
-string name = "Jenny ";
-string lastName = "Potter ";
-int age = 18;
-String fullName = "Hello " + name + lastName + "Your age is " + age;
-Console.WriteLine(fullName);
-
-bool ex1 = 20 > 6;
-Console.WriteLine(ex1);
-
-bool ex2 = 20 == 6;
-Console.WriteLine(ex2);
-
-bool ex3 = 20 < 6;
-Console.WriteLine(ex3);
-
-bool ex4 = 20 != 6;
-Console.WriteLine(ex4);
-
-bool ex5 = 20 >= 6;
-Console.WriteLine(ex5);
-
-bool ex6 = 20 <= 6;
-Console.WriteLine(ex6);
-
-bool result = true && true;
-Console.WriteLine(result);
-
-bool result2 = true || false;
-Console.WriteLine(result2);
-
-
+Console.WriteLine("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your last name: ");
+string lastName = Console.ReadLine();
+Console.WriteLine("Enter your age: ");
+string age = Console.ReadLine();
+Console.WriteLine("Enter your occupation: ");
+string occupation = Console.ReadLine();
+int convertedAge = int.Parse(age);
+Console.WriteLine("Hello " + name +" " + lastName + " "+ "I didn't think that you are " + convertedAge);
+Console.WriteLine(occupation + " is a very interesting job.");
 
--- obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net6.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b6f97b93cecf41116b318b4958e776c44d0e01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4c655da5d2c059786429e50aeabec09a92f3919")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
