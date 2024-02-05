4fbfb3b4148586fec2d20c3f826e0fe754ebcc8b
Nat386 <n.karasyova53@gmail.com>
2024-02-04 20:00:40 -0600

    if-switch

--- Program.cs
+++ Program.cs
@@ -1,13 +1,72 @@
 
-Console.WriteLine("Enter your name:");
-string name = Console.ReadLine();
-Console.WriteLine("Enter your last name: ");
-string lastName = Console.ReadLine();
-Console.WriteLine("Enter your age: ");
-string age = Console.ReadLine();
-Console.WriteLine("Enter your occupation: ");
-string occupation = Console.ReadLine();
-int convertedAge = int.Parse(age);
-Console.WriteLine("Hello " + name +" " + lastName + " "+ "I didn't think that you are " + convertedAge);
-Console.WriteLine(occupation + " is a very interesting job.");
+//Console.WriteLine("Enter your name:");
+//string name = Console.ReadLine();
+//Console.WriteLine("Enter your last name: ");
+//string lastName = Console.ReadLine();
+//Console.WriteLine("Enter your age: ");
+//string age = Console.ReadLine();
+//Console.WriteLine("Enter your occupation: ");
+//string occupation = Console.ReadLine();
+//int convertedAge = int.Parse(age);
+//Console.WriteLine("Hello " + name +" " + lastName + " "+ "I didn't think that you are " + convertedAge);
+//Console.WriteLine(occupation + " is a very interesting job.");
+
+int number = 50;
+
+if (number == 5)
+{ Console.WriteLine("your number is 5");}
+else
+{    Console.WriteLine("your number is not 5"); }
+
+if (number < 10)
+{ Console.WriteLine("your number is less than 10" ); }
+else
+{ Console.WriteLine("your number is bigger than 10"); }
+
+if(number <= 25)
+{    Console.WriteLine("your number is 25 or less");}
+else
+{    Console.WriteLine("your number is not 25 or less");}
+
+if (number == 50)
+{    Console.WriteLine("your number is 50");}
+else
+{    Console.WriteLine("your number is not equal 50");}
+
+if (number > 50)
+{    Console.WriteLine("your number is bigger than 50");}
+else
+{    Console.WriteLine("your number is not bigger than 50");}
+
+if (number !=100)
+{    Console.WriteLine("your number is not 100");}
+else
+{    Console.WriteLine("your number is 100");}
+
+int number2 = 25;
+
+switch (number2)
+{
+    case 5:
+        Console.WriteLine("your number is 5");
+        break;
+    case 10:
+        Console.WriteLine("your number is 10");
+        break;
+    case 25:
+        Console.WriteLine("your number is 25");
+        break;
+    case 50:
+        Console.WriteLine("your number is 50");
+        break;
+}
+
+
+
+
+
+
+
+
+
 
